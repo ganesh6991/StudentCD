152617202e40d0a743e9d57da0d146ee9ec150e9
ganesh6991 <41368879+ganesh6991@users.noreply.github.com>
2018-08-29 10:06:55 +0530

    created home page

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>	
+		<style type="text/css">
+			.home {
+			    text-align: center;
+			       margin-top:300px;
+				}
+			button.home-button {
+				    border: 0px;
+				    padding: 20px;
+				    margin-right: 34px;
+				    margin-left: 20px;
+				    border-radius: 7px;
+				    background-color: #bd581d;
+				    color: white;
+				}
+			.home p{
+				color: white;
+			    font-family: sans-serif;
+			    font-size: 67px;
+				}
+		</style>
+	</head>
+	<body style="background-image : url(images/home-background.jpg);background-size : cover;">
+		<div class="home">
+			<p>Student Details</p>
+			<a href="registration/index.php"><button class="home-button">Registration</button></a>
+			<a href="login/index.php"><button class="home-button">Login</button></a>
+		</div>
+	</body>
+</html>
